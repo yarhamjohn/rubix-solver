b02a4f3ee073c3758db17f799351e3de543ddab4
John Yarham <yarhamjohn@gmail.com>
2020-01-03 15:11:40 +0000

    more WIP

--- rubix-solver/Program.cs
+++ rubix-solver/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace rubix_solver
 {
@@ -52,7 +53,7 @@ namespace rubix_solver
                 },
                 {
                     {
-                        new Block(Colour.Red, null, Colour.Green, null, null, Colour.Yellow),
+                        new Block(Colour.Blue, null, Colour.Yellow, null, null, Colour.Orange),
                         new Block(null, null, Colour.Green, null, null, Colour.Yellow),
                         new Block(null, Colour.Green, Colour.Yellow, null, null, Colour.Orange)
                     },
@@ -62,16 +63,70 @@ namespace rubix_solver
                         new Block(null, Colour.Orange, null, null, null, Colour.Yellow)
                     },
                     {
-                        new Block(Colour.Yellow, null, null, Colour.Red, null, Colour.Blue),
+                        new Block(Colour.Yellow, null, null, Colour.Green, null, Colour.Red),
                         new Block(null, null, null, Colour.Blue, null, Colour.Yellow),
-                        new Block(null, Colour.Orange, null, Colour.Blue, null, Colour.Yellow)
+                        new Block(null, Colour.Yellow, null, Colour.Blue, null, Colour.Red)
                     }
                 }
             });
+            //
+            // var cube = new RubixCube(new[,,]
+            // {
+            //     {
+            //         {
+            //             new Block(Colour.Red, null, Colour.Green, null, Colour.White, null),
+            //             new Block(null, null, Colour.Green, null, Colour.White, null),
+            //             new Block(null, Colour.Orange, Colour.Green, null, Colour.White, null)
+            //         },
+            //         {
+            //             new Block(Colour.Red, null, null, null, Colour.White, null),
+            //             new Block(null, null, null, null, Colour.White, null),
+            //             new Block(null, Colour.Orange, null, null, Colour.White, null)
+            //         },
+            //         {
+            //             new Block(Colour.Red, null, null, Colour.Blue, Colour.White, null),
+            //             new Block(null, null, null, Colour.Blue, Colour.White, null),
+            //             new Block(null, Colour.Orange, null, Colour.Blue, Colour.White, null)
+            //         }
+            //     },
+            //     {
+            //         {
+            //             new Block(Colour.Red, null, Colour.Green, null, null, null),
+            //             new Block(null, null, Colour.Green, null, null, null),
+            //             new Block(null, Colour.Orange, Colour.Green, null, null, null)
+            //         },
+            //         {
+            //             new Block(Colour.Red, null, null, null, null, null),
+            //             new Block(null, null, null, null, null, null),
+            //             new Block(null, Colour.Orange, null, null, null, null)
+            //         },
+            //         {
+            //             new Block(Colour.Red, null, null, Colour.Blue, null, null),
+            //             new Block(null, null, null, Colour.Blue, null, null),
+            //             new Block(null, Colour.Orange, null, Colour.Blue, null, null)
+            //         }
+            //     },
+            //     {
+            //         {
+            //             new Block(Colour.Yellow, null, Colour.Red, null, null, Colour.Green),
+            //             new Block(null, null, Colour.Green, null, null, Colour.Yellow),
+            //             new Block(null, Colour.Green, Colour.Yellow, null, null, Colour.Orange)
+            //         },
+            //         {
+            //             new Block(Colour.Red, null, null, null, null, Colour.Yellow),
+            //             new Block(null, null, null, null, null, Colour.Yellow),
+            //             new Block(null, Colour.Orange, null, null, null, Colour.Yellow)
+            //         },
+            //         {
+            //             new Block(Colour.Red, null, null, Colour.Blue, null, Colour.Yellow),
+            //             new Block(null, null, null, Colour.Blue, null, Colour.Yellow),
+            //             new Block(null, Colour.Yellow, null, Colour.Orange, null, Colour.Blue)
+            //         }
+            //     }
+            // });
             
             cube.PrintCube();
-            
-            
+
             SolveThirdLayer(cube);
         }
 
@@ -79,12 +134,132 @@ namespace rubix_solver
         {
             // Form a cross on the back face
             // Correct the location of the middle edge pieces
+            
             // Get the corner pieces in the right corners
+            ReorganiseCorners(cube);
             
             // Correct the orientation of the corner pieces
             CorrectCornerOrientation(cube);
         }
 
+        private static void ReorganiseCorners(RubixCube cube)
+        {
+            // Can be 0, 1 or 4 correct blocks
+            var cornerBlocks = GetCornerBlocks(cube);
+            var incorrectCornerBlocks = cornerBlocks.Where(CornerBlockInWrongPosition).ToList();
+            
+            if (incorrectCornerBlocks.Count == 1 || incorrectCornerBlocks.Count == 2)
+            {
+                throw new Exception("Something has gone wrong. There can only be 0, 3 or 4 incorrect blocks at this stage.");
+            }
+
+            if (incorrectCornerBlocks.Count == 0)
+            {
+                return;
+            }
+            
+            while (GetCornerBlocks(cube).Where(CornerBlockInWrongPosition).ToList().Count > 0)
+            {
+                if (incorrectCornerBlocks.Count == 3)
+                {
+                    var ((x, y), _) = cornerBlocks.Except(incorrectCornerBlocks).First();
+                    switch (x, y)
+                    {
+                        case (0, 0):
+                        {
+                            PerformUrulRotations(cube, Layer.Top, Layer.Bottom);
+                            break;
+                        }
+                        case (0, 2):
+                        {
+                            PerformUrulRotations(cube, Layer.Right, Layer.Left);
+                            break;
+                        }
+                        case (2, 0):
+                        {
+                            PerformUrulRotations(cube, Layer.Left, Layer.Right);
+                            break;
+                        }
+                        case (2, 2):
+                        {
+                            PerformUrulRotations(cube, Layer.Bottom, Layer.Top);
+                            break;
+                        }
+                        default:
+                            throw new Exception($"Cannot perform urul rotation as target is not a corner block: ({x}, {y})");
+                    }
+                }
+                else
+                {
+                    PerformUrulRotations(cube, Layer.Top, Layer.Bottom);
+                }
+            }
+        }
+
+        private static Dictionary<(int, int), Block> GetCornerBlocks(RubixCube cube)
+        {
+            var face = cube.GetFace(Layer.Back);
+            return new Dictionary<(int, int), Block>
+            {
+                {(0, 0), face[0, 0]},
+                {(0, 2), face[0, 2]},
+                {(2, 0), face[2, 0]},
+                {(2, 2), face[2, 2]}
+            };
+        }
+
+        private static void PerformUrulRotations(RubixCube cube, Layer rightSide, Layer leftSide)
+        {
+            Console.WriteLine("Begin Urul Urul rotation");
+            
+            cube.RotateAntiClockwise(Layer.Back);
+            cube.PrintCube();
+            cube.RotateAntiClockwise(rightSide);
+            cube.PrintCube();
+            cube.RotateClockwise(Layer.Back);
+            cube.PrintCube();
+            cube.RotateAntiClockwise(leftSide);
+            cube.PrintCube();
+
+            cube.RotateAntiClockwise(Layer.Back);
+            cube.PrintCube();
+            cube.RotateClockwise(rightSide);
+            cube.PrintCube();
+            cube.RotateClockwise(Layer.Back);
+            cube.PrintCube();
+            cube.RotateClockwise(leftSide);
+            cube.PrintCube();
+        }
+
+        private static bool CornerBlockInWrongPosition(KeyValuePair<(int, int), Block> cornerBlock)
+        {
+            var ((x, y), block) = cornerBlock;
+            switch (x, y)
+            {
+                case (0, 0):
+                {
+                    var blockFaces = new List<Colour?> {block.Back, block.Left, block.Top};
+                    return !blockFaces.Contains(Colour.Yellow) || !blockFaces.Contains(Colour.Red) || !blockFaces.Contains(Colour.Green);
+                }
+                case (0, 2):
+                {
+                    var blockFaces = new List<Colour?> {block.Back, block.Right, block.Top};
+                    return !blockFaces.Contains(Colour.Yellow) || !blockFaces.Contains(Colour.Orange) || !blockFaces.Contains(Colour.Green);
+                }
+                case (2, 0):
+                {
+                    var blockFaces = new List<Colour?> {block.Back, block.Left, block.Bottom};
+                    return !blockFaces.Contains(Colour.Yellow) || !blockFaces.Contains(Colour.Red) || !blockFaces.Contains(Colour.Blue);
+                }
+                case (2, 2):
+                {
+                    var blockFaces = new List<Colour?> {block.Back, block.Right, block.Bottom};
+                    return !blockFaces.Contains(Colour.Yellow) || !blockFaces.Contains(Colour.Orange) || !blockFaces.Contains(Colour.Blue);
+                }
+                default:
+                    throw new Exception($"Cannot determine if corner block is an incorrect position as it is not a corner block: ({x}, {y})");
+            }
+        }
 
         private static void CorrectCornerOrientation(RubixCube cube)
         {
@@ -111,13 +286,16 @@ namespace rubix_solver
 
             while (!cube.IsSolved())
             {
+
                 var currentFace = cube.GetFace(Layer.Back);
                 if (currentFace[x, y].Back == Colour.Yellow)
                 {
+                    Console.WriteLine($"Reorientate for RDRD rotation");
                     cube.RotateClockwise(Layer.Back);
                 }
                 else
                 {
+                    Console.WriteLine($"Begin RDRD rotation to solve {x}, {y}");
                     var sideToRotate = (x, y) switch
                     {
                         (0, 0) => Layer.Top, 
@@ -127,10 +305,16 @@ namespace rubix_solver
                         _ => throw new Exception($"Can't correct corner orientation in the third layer as it is not a corner piece: ({x}, {y})")
                     };
                     
-                    cube.RotateClockwise(sideToRotate);
-                    cube.RotateAntiClockwise(Layer.Front);
+                    // Does this change? I.e. enticlockwise for top/bottom, clockwise for left right? I think so! Need some tests - can't rely on manual testing. Think my clockwise/eanticloswise movements are wrong?
                     cube.RotateAntiClockwise(sideToRotate);
+                    cube.PrintCube();
+
+                    cube.RotateAntiClockwise(Layer.Front);
+                    cube.PrintCube();
+                    cube.RotateClockwise(sideToRotate);
+                    cube.PrintCube();
                     cube.RotateClockwise(Layer.Front);
+                    cube.PrintCube();
                 }
 
                 cube.PrintCube();
